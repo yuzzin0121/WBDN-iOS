a34813ab0cda62bb06e92d8af1434ef61818287d
qwerty3345 <qwerty3345@naver.com>
2023-11-25 18:03:56 +0900

    [Feat] 그라데이션 배경 색상 조절

--- WBDN/WBDN/Sources/Scene/Map/MapViewController.swift
+++ WBDN/WBDN/Sources/Scene/Map/MapViewController.swift
@@ -10,6 +10,6 @@ import UIKit
 final class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        applyGradientBackground()
     }
 }
