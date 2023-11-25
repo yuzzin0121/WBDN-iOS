92dd84c0dfa7e455524212ce89e768386f7cf71b
qwerty3345 <qwerty3345@naver.com>
2023-11-25 15:38:36 +0900

    [Build] 프로젝트 초기 생성

---
+++ WBDN/WBDN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBDN
+//
+//  Created by Mason Kim on 11/25/23.
+//a
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
