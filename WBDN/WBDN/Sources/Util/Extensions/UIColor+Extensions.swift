9e7d8060f75fba9df4a6a4510e9c123bf028fdac
yuzzin0121 <ydbwlsjki60@gmail.com>
2023-11-26 02:21:53 +0900

    [Feat] PhotoInfoViewController 추가

--- WBDN/WBDN/Sources/Util/Extensions/UIColor+Extensions.swift
+++ WBDN/WBDN/Sources/Util/Extensions/UIColor+Extensions.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 extension UIColor {
-    class var mainNavyColor: UIColor? { return UIColor(named: "mainNavyColor")}
+    class var customNavy: UIColor? { return UIColor(named: "customNavy")}
+    class var customLightNavy: UIColor? { return UIColor(named: "customLightNavy")}
+    class var customRed: UIColor? { return UIColor(named: "customRed")}
+    class var customGray: UIColor? { return UIColor(named: "customGray")}
+    class var customSkyBlue: UIColor? { return UIColor(named: "customSkyBlue")}
+    class var customYellow: UIColor? { return UIColor(named: "customYellow")}
 }
---
+++ WBDN/WBDN/Sources/Util/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIImage+Extensions.swift
+//  WBDN
+//
+//  Created by 조유진 on 2023/11/26.
+//
+
+import UIKit
+
+extension UIImage {
+    static let mainHeaderStars = UIImage(named: "mainHeaderStars")
+}
